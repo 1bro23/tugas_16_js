8cac962746d247e8aed1a07be0d964580e24f5b4
Ramdhawa <shinlia21@gmail.com>
2020-08-27 10:16:21 +0800

    add tugas 16 js

---
+++ tugas16.js
@@ -0,0 +1,4 @@
+var angkamin=Math.min(45,100,99,3,2,4,1,2,3,19,22,14);
+console.log("Nilai Minimal :",angkamin);
+var angkamax=Math.max(45,100,99,3,2,4,1,2,3,19,22,14);
+console.log("Nilai Maksimal :",angkamax);
